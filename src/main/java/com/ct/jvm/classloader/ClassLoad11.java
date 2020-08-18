1ede4b7d9f3cfc65d9247d4b1e1ddd9184c13afc
畅通 <changtong@MacBook-Pro-3.local>
2020-08-18 14:09:23 +0800

    fix bug

--- src/main/java/com/ct/jvm/classloader/ClassLoad11.java
+++ src/main/java/com/ct/jvm/classloader/ClassLoad11.java
@@ -1,5 +1,9 @@
 package com.ct.jvm.classloader;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * @author changtong
  * @since 2020/4/7
@@ -7,15 +11,22 @@ package com.ct.jvm.classloader;
 public class ClassLoad11 {
 
     public static void main(String[] args) throws Exception {
-        CustomClassLoader classLoader = new CustomClassLoader("myCustomClassLoader");
-        classLoader.setPath("/Users/changtong/Downloads/testclassload/");
-        Class<?> clazz = classLoader.loadClass("com.ct.jvm.classloader.MySimple");
-        System.out.println(clazz.hashCode());
-        //如果注释掉该行，就并不会实例化MySimple对象，不会加载MyCat（可能预先加载）
-        Object instance = clazz.newInstance();//实列化Simple和MyCat
-        //MyCat是由加载MySample的加载器去加载的：
-        //如果只删除classpath下的MyCat，则会报错，NoClassDefFoundError；
-        //如果只删除classpath下的MySample，则由自定义加载器加载桌面上的MySample，由系统应用加载器加载MyCat。
+//        CustomClassLoader classLoader = new CustomClassLoader("myCustomClassLoader");
+//        classLoader.setPath("/Users/changtong/Downloads/testclassload/");
+//        Class<?> clazz = classLoader.loadClass("com.ct.jvm.classloader.MySimple");
+//        System.out.println(clazz.hashCode());
+//        //如果注释掉该行，就并不会实例化MySimple对象，不会加载MyCat（可能预先加载）
+//        Object instance = clazz.newInstance();//实列化Simple和MyCat
+//        //MyCat是由加载MySample的加载器去加载的：
+//        //如果只删除classpath下的MyCat，则会报错，NoClassDefFoundError；
+//        //如果只删除classpath下的MySample，则由自定义加载器加载桌面上的MySample，由系统应用加载器加载MyCat。
+
+        long time = System.currentTimeMillis();
+        System.out.println(time);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        long time1 = sdf.parse(sdf.format(time)).getTime();
+        System.out.println(time1);
+
     }
 
 
